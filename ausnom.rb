ae9ee6fadfed215c4b7ced4b0f2e29c7f34a54bf
Xavier Shay <xavier@rhnh.net>
2011-01-24 14:58:14 +1100

    Moved popular nutrients to the top, general clean up

--- ausnom.rb
+++ ausnom.rb
@@ -2,13 +2,18 @@ require 'sinatra'
 require 'json'
 require './models'
 
-LAST_MODIFIED = Time.mktime(2011,1,20).httpdate
+LAST_MODIFIED = Time.mktime(2011,1,24).httpdate
+TOP_NUTRIENTS = %w(ENERGY-04DF PROT FE CA FAT)
 
 get '/' do
   last_modified LAST_MODIFIED
   @q = params[:q]
   @foods = @q ? Food.search(@q) : []
-  erb :index
+  if @foods.length == 1
+    redirect "/foods/#{@foods[0].food_id}"
+  else
+    erb :index
+  end
 end
 
 get '/foods/:id.json' do
@@ -22,10 +27,31 @@ end
 get '/foods/:id' do
   last_modified LAST_MODIFIED
   @food = Food.get!(params[:id])
+  all_nutrients = @food.nutrients.all(:order => [:description.asc])
+  @top_nutrients = all_nutrients.select {|x| TOP_NUTRIENTS.include?(x.nutrient_id) }.sort_by {|x| TOP_NUTRIENTS.index(x.nutrient_id) }
+  @other_nutrients = all_nutrients - @top_nutrients
   erb :show
 end
 
 helpers do
   include Rack::Utils
   alias_method :h, :escape_html
+
+  def format_value v
+    if v > 10
+      v.round
+    else
+      v
+    end
+  end
+
+  def page_title
+    if @food
+      @food.name
+    elsif @q
+      "Search for '#{@q}'"
+    else
+      "AU and NZ nutrional data web service"
+    end
+  end
 end
