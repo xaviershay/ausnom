e831a841f74cd3729b4e596e0dc5670b0ed94acc
Xavier Shay <xavier@rhnh.net>
2011-01-24 14:24:13 +1100

    Last-modified on everything, remove expires headers

--- ausnom.rb
+++ ausnom.rb
@@ -5,13 +5,13 @@ require './models'
 LAST_MODIFIED = Time.mktime(2011,1,20).httpdate
 
 get '/' do
+  last_modified LAST_MODIFIED
   @q = params[:q]
   @foods = @q ? Food.search(@q) : []
   erb :index
 end
 
 get '/foods/:id.json' do
-  header 'Expires' => (Time.now + 60*60*24*365*3).httpdate
   last_modified LAST_MODIFIED
   @food = Food.get!(params[:id])
   @food.attributes.merge(
@@ -20,7 +20,6 @@ get '/foods/:id.json' do
 end
 
 get '/foods/:id' do
-  headers 'Expires' => (Time.now + 60*60*24*365*3).httpdate
   last_modified LAST_MODIFIED
   @food = Food.get!(params[:id])
   erb :show
